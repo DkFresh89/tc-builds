caa5bcf773fe9b2c2f77ec1f82e4e8ed70aa2ebf
DkFresh89 <me@dougkerzner.dev>
2021-09-02 12:29:58 -0400

    Installed React Icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { Box, Divider, Flex, Spacer, Stack, Image } from '@chakra-ui/react'
 import './App.css';
 import logo from './TCLogo.svg'
@@ -18,16 +17,12 @@ function App() {
       py={8}
     >
       <Box mb={8}>
-        {/* <Logo /> */}
         <Image src={logo} />
       </Box>
-      {/* <SearchField mb={6} /> */}
       <Stack spacing={6}>
         <Stack>
           {/* <NavLink label="People" icon={FaUser} isActive />
-          <NavLink label="Favorites" icon={FaRegHeart} />
-          <NavLink label="Workflow" icon={FaRegPaperPlane} />
-          <NavLink label="Statistics" icon={FaRegChartBar} /> */}
+          <NavLink label="Favorites" icon={FaRegHeart} /> */}
         </Stack>
         <Divider borderColor="whiteAlpha.400" />
         <Stack>
@@ -36,11 +31,6 @@ function App() {
         </Stack>
       </Stack>
       <Spacer />
-      {/* <UserProfile
-        name="Cindy Winston"
-        image="https://images.unsplash.com/photo-1521296797187-726205347ca9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fGxhZHklMjBzbWlsaW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-        email="cindy@example.com"
-      /> */}
     </Flex>
   );
 }
