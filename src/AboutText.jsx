cc6c6c1bbfb2d9816ee04e4ec3c903eab0574347
DkFresh89 <me@dougkerzner.dev>
2021-09-09 11:43:13 -0400

    List icons complete

--- src/AboutText.jsx
+++ src/AboutText.jsx
@@ -1,7 +1,7 @@
 import { Text, Link, List, ListItem, ListIcon, Box } from "@chakra-ui/react";
 import { Link as ReachLink } from "react-router-dom";
 import {  } from "react-icons/go";
-import { FcIdea, FcEngineering } from "react-icons/fc";
+import { FcIdea, FcEngineering, FcDeployment, FcDocument, FcElectronics, FcGlobe, FcCircuit, FcFactory } from "react-icons/fc";
 
 
 export default function AboutText() {
@@ -10,20 +10,36 @@ export default function AboutText() {
     return(
         <Box>
 
-        <Text>TC Builds was established in 2020 as a fully vertical engineering resource. We can assist at any stage of development from but not limited to: </Text>
-            <List>
+        <Text margin='1'>TC Builds was established in 2020 as a fully vertical engineering resource. We can assist at any stage of development from but not limited to: </Text>
+            <List spacing='2.5'>
                 <ListItem>
                     <ListIcon as={FcIdea}/>
                     Back of the napkin idea
                 </ListItem>
-                <ListItem>Full design</ListItem>
-                <ListItem>Assebmly</ListItem>
-                <ListItem>Prototyping</ListItem>
-                <ListItem>Assebmley</ListItem>
-                <ListItem>Testing/Debugging</ListItem>
-                <ListItem>Documentation</ListItem>
-                <ListItem>Global sourcing</ListItem>
-                <ListItem>Product launch</ListItem>
+                <ListItem>
+                    <ListIcon as={FcCircuit}/>
+                    Full design
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={FcEngineering}/>
+                    Prototyping
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={FcFactory}/>
+                    Assembley
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={FcElectronics}/>
+                    Testing/Debugging</ListItem>
+                <ListItem>
+                    <ListIcon as={FcDocument}/>
+                    Documentation</ListItem>
+                <ListItem>
+                    <ListIcon as={FcGlobe}/>
+                    Global sourcing</ListItem>
+                <ListItem>
+                    <ListIcon as={FcDeployment}/>
+                    Product launch</ListItem>
                 <ListItem></ListItem>
             </List>
             <Text>We are with you every step of the way. If you have any questions, ideas, or would just like to drop us a line, please visit our <Link as={ReachLink} to='/contact' color='blue' >contact us</Link>  page!</Text>
